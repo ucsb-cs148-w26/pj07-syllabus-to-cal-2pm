8196b2d8aa746077242a045acfca9f1f06b71e18
Matt Blanke <mattheweblanke@gmail.com>
2026-01-18 21:24:07 -0800

    mb - Add HelloWorld SwiftUI app

---
+++ HelloWorld/HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Matthew Blanke on 1/18/26.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
