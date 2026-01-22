9a681619f00774ab22460eb414e32c2225c40a20
Divya Subramonian <divyasubramonian@ucsb.edu>
2026-01-22 01:06:53 -0800

    ds - added content view for homepage and deleted hello world

---
+++ SyllabusToCalendar/SyllabusToCalendarTests/SyllabusToCalendarTests.swift
@@ -0,0 +1,17 @@
+//
+//  SyllabusToCalendarTests.swift
+//  SyllabusToCalendarTests
+//
+//  Created by Divya Subramonian on 1/21/26.
+//
+
+import Testing
+@testable import SyllabusToCalendar
+
+struct SyllabusToCalendarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
